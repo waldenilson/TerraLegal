697163db5787a5f0a799235eb0990f7aa7a5b0dd
waldenilson.carneiro <waldenilson.carneiro@msls238784.sls.incra.gov.br>
2013-07-29 12:04:44 -0300

    models do banco de dados newsicop

--- SICOP/SICOP/settings.py
+++ SICOP/SICOP/settings.py
@@ -20,7 +20,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'django',                      # Or path to database file if using sqlite3.
+        'NAME': 'newsicop',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'admin',
         'PASSWORD': '123456',
--- SICOP/SICOP/urls.py
+++ SICOP/SICOP/urls.py
@@ -7,6 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'web.views.home', name='home'),
+    url(r'^web/$', 'web.views.txt', name='texto'),
     # url(r'^SICOP/', include('SICOP.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
