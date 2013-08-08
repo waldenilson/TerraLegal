52cdca6e39275a6da97a7ec26cabf1a47d177c4b
waldenilson.carneiro <waldenilson.carneiro@msls238784.sls.incra.gov.br>
2013-08-08 08:42:22 -0300

    teste comit

--- SICOP/web/views.py
+++ SICOP/web/views.py
@@ -2,8 +2,12 @@
 from django.template.context import Context
 from django.template import loader
 from django.http.response import HttpResponse
+from django.shortcuts import render_to_response
 
 def home(request):
     template = loader.get_template("base.html")
     c = Context ({})
     return HttpResponse(template.render(c))
+
+def search(request):
+    return render_to_response()
