ee6471d35643ff8d6b72398a51cb9dbda603e0aa
wcarneiro <wal.denilson@hotmail.com>
2013-09-02 23:11:50 -0300

    cadastro, edicao, validacao , consulta de municipio-modulo

--- SICOP/sicop/restrito/municipio_modulo.py
+++ SICOP/sicop/restrito/municipio_modulo.py
@@ -1,24 +1,27 @@
-#PROCESSOS -----------------------------------------------------------------------------------------------------------------------------
 from django.contrib.auth.decorators import login_required, permission_required
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template.context import RequestContext
 from sicop.forms import FormProcessos, FormMunicipioModulo
-from sicop.models import Tbmunicipiomodulo
+from sicop.models import Tbmunicipiomodulo, Tbmunicipio
 from django.contrib import messages
 from django.http.response import HttpResponseRedirect
 
 @login_required
 def consulta(request):
+    municipio = Tbmunicipio.objects.all()
     if request.method == "POST":
-        nrmodulorural = request.POST['nrmodulorural']
-        lista = Tbmunicipiomodulo.objects.all().filter( nrmodulorural__contains=nrmodulorural )
+        mun = request.POST['tbmunicipio']
+        lista = Tbmunicipiomodulo.objects.all()
+        if mun != '0':
+            lista = lista.filter( tbmunicipio = mun )
     else:
         lista = Tbmunicipiomodulo.objects.all()
     lista = lista.order_by( 'id' )
-    return render_to_response('sicop/restrito/municipio_modulo/consulta.html' ,{'lista':lista}, context_instance = RequestContext(request))
+    return render_to_response('sicop/restrito/municipio_modulo/consulta.html' ,{'lista':lista,'municipio':municipio}, context_instance = RequestContext(request))
 
 @login_required
 def cadastro(request):
+    municipio = Tbmunicipio.objects.all()
     if request.method == "POST":
         form = FormMunicipioModulo(request.POST)
         if validacao(request):
@@ -27,10 +30,11 @@ def cadastro(request):
                 return HttpResponseRedirect("/sicop/restrito/municipio_modulo/consulta/") 
     else:
         form = FormMunicipioModulo()
-    return render_to_response('sicop/restrito/municipio_modulo/cadastro.html',{"form":form}, context_instance = RequestContext(request))
+    return render_to_response('sicop/restrito/municipio_modulo/cadastro.html',{"form":form,'municipio':municipio}, context_instance = RequestContext(request))
 
 @login_required
 def edicao(request, id):
+    municipio = Tbmunicipio.objects.all()
     instance = get_object_or_404(Tbmunicipiomodulo, id=id)
     if request.method == "POST":
         form = FormMunicipioModulo(request.POST,request.FILES,instance=instance)
@@ -40,7 +44,7 @@ def edicao(request, id):
                 return HttpResponseRedirect("/sicop/restrito/municipio_modulo/consulta/")
     else:
         form = FormMunicipioModulo(instance=instance) 
-    return render_to_response('sicop/restrito/municipio_modulo/edicao.html', {"form":form}, context_instance = RequestContext(request))
+    return render_to_response('sicop/restrito/municipio_modulo/edicao.html', {"form":form,'municipio':municipio}, context_instance = RequestContext(request))
 
 def validacao(request_form):
     warning = True
