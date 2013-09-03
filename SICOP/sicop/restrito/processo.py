4b962c8a3e52173cf0db68a141f3ea2c5b0279f2
wcarneiro <wal.denilson@hotmail.com>
2013-09-03 20:46:09 -0300

    consulta de processos dinamica sem a necessidade de escolher um tipo de processo.

--- SICOP/sicop/restrito/processo.py
+++ SICOP/sicop/restrito/processo.py
@@ -3,43 +3,44 @@ from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 from django.http.response import HttpResponseRedirect
 from sicop.models import Tbprocessorural, Tbtipoprocesso, Tbprocessourbano,\
-    Tbprocessoclausula
+    Tbprocessoclausula, Tbprocessobase
 from sicop.forms import FormProcessoRural, FormProcessoUrbano,\
     FormProcessoClausula
 
 @login_required
 def consulta(request):
-    tipoprocesso = Tbtipoprocesso.objects.all()
-    escolha = "tbprocessorural"
-    div_processo = "rural"
-    lista = Tbprocessorural.objects.all()
-    
+    lista = Tbprocessobase.objects.all().filter( id = "0" )
     if request.method == "POST":
         escolha = request.POST['escolha']
-        if escolha == "tbprocessorural":
-            div_processo = "rural"
-            return render_to_response('sicop/restrito/processo/consulta.html',
-                    {'tipoprocesso':tipoprocesso,'processo':escolha,
-                    'div_processo':div_processo,'lista':lista},
-                    context_instance = RequestContext(request));
+        pesquisa = request.POST['pesquisa']
+        if escolha == "numero":
+            lista = Tbprocessobase.objects.all().filter( nrprocesso__contains = pesquisa )
         else:
-            if escolha == "tbprocessourbano":
-                div_processo = "urbano"
-                lista = Tbprocessourbano.objects.all()
-                return render_to_response('sicop/restrito/processo/consulta.html',
-                    {'tipoprocesso':tipoprocesso,'processo':escolha,
-                    'div_processo':div_processo,'lista':lista},
-                    context_instance = RequestContext(request));
+            if escolha == "nome":
+                p_rural = Tbprocessorural.objects.all().filter( nmrequerente__contains = pesquisa )
+                p_clausula = Tbprocessoclausula.objects.all().filter( nmrequerente__contains = pesquisa )
+                lista = []
+                for obj in p_rural:
+                    lista.append( obj.tbprocessobase )
+                for obj in p_clausula:
+                    lista.append( obj.tbprocessobase )
             else:
-                if escolha == "tbprocessoclausula":
-                    div_processo = "clausula"
-                    lista = Tbprocessoclausula.objects.all()
-                    return render_to_response('sicop/restrito/processo/consulta.html',
-                    {'tipoprocesso':tipoprocesso,'processo':escolha,
-                    'div_processo':div_processo,'lista':lista},
-                    context_instance = RequestContext(request));  
-    
-    return render_to_response('sicop/restrito/processo/consulta.html',{'tipoprocesso':tipoprocesso,'processo':escolha,'div_processo':div_processo,'lista':lista}, context_instance = RequestContext(request))
+                if escolha == "cpf":
+                    p_rural = Tbprocessorural.objects.all().filter( nrcpfrequerente__contains = pesquisa )
+                    p_clausula = Tbprocessoclausula.objects.all().filter( nrcpfrequerente__contains = pesquisa )
+                    lista = []
+                    for obj in p_rural:
+                        lista.append( obj.tbprocessobase )
+                    for obj in p_clausula:
+                        lista.append( obj.tbprocessobase )
+                else:
+                    if escolha == "cnpj":
+                        p_urbano = Tbprocessourbano.objects.all().filter( nrcnpj__contains = pesquisa ) 
+                        lista = []
+                        for obj in p_urbano:
+                            lista.append( obj.tbprocessobase )
+        
+    return render_to_response('sicop/restrito/processo/consulta.html',{'lista':lista}, context_instance = RequestContext(request))
    
     
 @login_required
