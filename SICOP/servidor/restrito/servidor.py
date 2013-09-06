8d3bdbc940ed6bc3454542b4fd27bedb193b4907
Eduardo Aguilar de Oliveira <edu.saoluis@gmail.com>
2013-09-06 17:44:15 -0300

    Teste de commit

--- SICOP/servidor/restrito/servidor.py
+++ SICOP/servidor/restrito/servidor.py
@@ -1,3 +1,4 @@
+#Visoes para controle de servidores
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponseRedirect
