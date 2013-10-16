091bf3a6b0c363452c9ff0dfd11501541966ac9d
waldenilson.carneiro <waldenilson.carneiro@msls238784.sls.incra.gov.br>
2013-10-16 11:03:06 -0300

    Dados da tabela contrato migrados perfeitamente.

---
+++ SICOP/staticfiles/migracao/dump/scriptTbcontrato.sql
@@ -0,0 +1,11 @@
+INSERT INTO tbcontrato values( '12000','Engeplagri',1 );
+INSERT INTO tbcontrato values( '22400','Engeplagri',1 );
+INSERT INTO tbcontrato values( 'INCRA','INCRA',1 );
+INSERT INTO tbcontrato values( '35/2012','Progeto',1 );
+INSERT INTO tbcontrato values( '19600','SETA',1 );
+INSERT INTO tbcontrato values( '006','SETA',1 );
+INSERT INTO tbcontrato values( '001','GERAL',1 );
+INSERT INTO tbcontrato values( '11100/2009','SETA',1 );
+INSERT INTO tbcontrato values( 'pendente','pendente',1 );
+INSERT INTO tbcontrato values( '53/2009','a definir',1 );
+
