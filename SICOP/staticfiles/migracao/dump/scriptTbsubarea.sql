ceeaeba1f02ff1ba2cdd82c043be04879b9c2211
waldenilson.carneiro <waldenilson.carneiro@msls238784.sls.incra.gov.br>
2013-10-16 11:34:43 -0300

    Migracoes das tabelas tbsubarea, tbtipopendencia.

---
+++ SICOP/staticfiles/migracao/dump/scriptTbsubarea.sql
@@ -0,0 +1,7 @@
+INSERT INTO tbsubarea values( '1','01',1 );
+INSERT INTO tbsubarea values( '2','02',1 );
+INSERT INTO tbsubarea values( '3','03',1 );
+INSERT INTO tbsubarea values( '4','-',1 );
+INSERT INTO tbsubarea values( '5','Loteamento Buritirana',1 );
+INSERT INTO tbsubarea values( '6','Loteamento Cobra',1 );
+
