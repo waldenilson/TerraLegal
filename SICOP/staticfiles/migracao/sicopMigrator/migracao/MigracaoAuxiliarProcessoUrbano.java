ef52c1cffbc491f117d6444bf8d438ce204d439c
waldenilson <wal.denilson@hotmail.com>
2013-11-08 11:24:43 -0300

    SicopMigrator: arquivo MigracaoAuxiliarProcessoUrbano para capturar os dados de pregao e situacaogeo

---
+++ SICOP/staticfiles/migracao/sicopMigrator/migracao/MigracaoAuxiliarProcessoUrbano.java
@@ -0,0 +1,140 @@
+package br.gov.incra.migracao;
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class MigracaoAuxiliarProcessoUrbano {
+
+	private String diretorio = "/opt/DEVELOPER/SICOP/Migracao";
+	private String nomeArqMigracao = "scriptTbAuxProcessoUrbano.sql";
+	private String nomeArqLegado = "dump_tbprocessourbano.txt";
+	
+	public MigracaoAuxiliarProcessoUrbano()
+	{		
+		File dir = new File( diretorio );
+		String todosScripts = "";
+			String conteudo = ler(diretorio, nomeArqLegado);
+			todosScripts += conteudo;
+		escreve(todosScripts, new File(dir, nomeArqMigracao) );
+	}
+	
+	public static void main(String a[])
+	{
+		new MigracaoAuxiliarProcessoUrbano();
+	}
+	
+	
+	public String ler(String diretorio, String nomeArq)
+	{
+		
+			File dir = new File( diretorio );
+			File arq = new File(dir, nomeArq );
+		 	
+			
+			try {
+		        //Indicamos o arquivo que será lido
+		        FileReader fileReader = new FileReader(arq);
+		 
+		        //Criamos o objeto bufferReader que nos
+		        // oferece o método de leitura readLine()
+		        BufferedReader bufferedReader =
+		            new BufferedReader(fileReader);
+		 
+		        //String que irá receber cada linha do arquivo
+		        String linha = "";
+		        String conteudo = "";
+		        String tabela = nomeArq.substring(5, nomeArq.length()-4);
+	//	        System.out.println("table: "+tabela);
+		        String leitura = "";
+		        
+		        
+		        
+		        //Fazemos um loop linha a linha no arquivo,
+		        // enquanto ele seja diferente de null.
+		        //O método readLine() devolve a linha na
+		        // posicao do loop para a variavel linha.
+		        int x = 1;
+		        while ( ( linha = bufferedReader.readLine() ) != null) {
+		            
+		        	//Aqui imprimimos a linha
+	//	        	tabela = "tbconjuge";
+		        	
+	//	        	String cont = linha.replaceAll("\t", ",");
+		        	String cont = linha;
+		        	String contaux = "";
+		        	String[] s = cont.split(";");
+		        	for(int y=0; y < s.length ; y++)
+		        	{
+		        		String aux = "";
+		        		String a = s[y];
+		        		
+		        		a = a.replaceAll("\"", "");
+		        		a = a.trim();
+		        		
+		        		if (a.startsWith("20") && a.contains("-") && a.contains(":"))
+		        			aux = "'"+a+"'";
+	
+		        		else if (a.startsWith("{") && a.endsWith("}") )
+		        			aux = "'"+a+"'";
+		        		
+		        		else
+		        		
+		        			aux = "'"+a+"'";
+		        		
+		        		if(y==7)
+		        			aux = "null";
+		        			contaux += aux+"\t";
+		        		
+		        	}
+		        	cont = contaux.substring(0,contaux.length()-1);
+		        	
+	//        		cont = cont.replaceAll("\t\t", "\t");
+	        		cont = cont.replaceAll("\t", ",");
+	        		cont = cont.replaceAll(",,", ",null,");
+	        		cont = cont.replaceAll(",,", ",null,");
+	        		cont = cont.replaceAll("''", "null");
+		        	
+//		        	System.out.println( cont );
+	//	        	System.exit(0);
+	//	        	cont = cont.replaceAll("\t", ",");
+	        		tabela = "tbprocessourbano";
+		        	conteudo += "INSERT INTO "+tabela+" values( "+cont+" );\n";
+		        	leitura += x+"\t"+"INSERT INTO "+tabela+" values( "+cont+" );\n";
+		        	x++;
+	//	            System.out.println(conteudo);
+		        }
+		        System.out.println("conteudo: "+leitura);
+		        
+	//	        escreve(conteudo, new File(dir, "dump_"+tabela+"_db.sql") );
+		 
+		        //liberamos o fluxo dos objetos
+		        // ou fechamos o arquivo
+		        fileReader.close();
+		        bufferedReader.close();
+		        return conteudo;
+		    } catch (IOException e) {
+		        e.printStackTrace();
+		        return null;
+		    }
+
+	}
+	
+	// método para escrever no TXT  
+    public void escreve (String conteudo, File arq){  
+          
+        try {  
+            FileWriter escreve = new FileWriter(arq, true);  
+            conteudo += System.getProperty("line.separator"); // pular linha  
+            escreve.write(conteudo); // escrever o conteúdo  
+            escreve.close();  
+        } catch (IOException e) {  
+            // TODO Auto-generated catch block  
+            e.printStackTrace();  
+        }  
+    }  
+		
+}
