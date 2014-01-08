3965f3d7c34839b377443e8d0fcec87640c2197b
Eduardo Aguilar de Oliveira <edu.saoluis@gmail.com>
2014-01-08 14:33:29 -0300

    Modulo Servidor completo

--- SICOP/servidor/restrito/servidor.py
+++ SICOP/servidor/restrito/servidor.py
@@ -3,10 +3,11 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.template.context import RequestContext
 from django.contrib import messages
+
 from sicop.forms import FormPecasTecnicas
 from sicop.forms import FormServidor
 
-from sicop.models import Tbpecastecnicas, Tbgleba, Tbcaixa, Tbcontrato, Tbservidor
+from sicop.models import Tbpecastecnicas, Tbgleba, Tbcaixa, Tbcontrato, Tbservidor, AuthUser, Tbdivisao
 from sicop.admin import verificar_permissao_grupo
 from django.http.response import HttpResponse
 from sicop.relatorio_base import relatorio_pdf_base_header,\
@@ -24,19 +25,13 @@ planilha_relatorio  = "Servidores"
 @login_required
 def consulta(request):
     if request.method == "POST":
-        #requerente = request.POST['requerente']
-        #cpf = request.POST['cpf']
-        #entrega = request.POST['entrega']
         servidor = request.POST['servidor']
-        #lista = Tbpecastecnicas.objects.all().filter( nmrequerente__contains=requerente, nrcpfrequerente__contains=cpf, nrentrega__contains=entrega )
         lista = Tbservidor.objects.all().filter( nmservidor__icontains=servidor)
-    
     else:
         lista = Tbservidor.objects.all()
-    lista = lista.order_by( 'id' )
+    lista = lista.order_by( 'nmservidor' )
     #gravando na sessao o resultado da consulta preparando para o relatorio/pdf
     request.session['relatorio_servidor'] = lista
-    #return render_to_response('sicop/restrito/peca_tecnica/consulta.html' ,{'lista':lista}, context_instance = RequestContext(request))
     return render_to_response('controle/servidor/consulta.html' ,{'lista':lista}, context_instance = RequestContext(request))
 
 @login_required
@@ -46,19 +41,30 @@ def cadastro(request):
     #contrato = Tbcontrato.objects.all()
     #caixa = Tbcaixa.objects.filter( tbtipocaixa = 2 )
     #gleba = Tbgleba.objects.all()
+    divisao = Tbdivisao.objects.all()
     if request.method == "POST":
-        form = FormServidor(request.POST)
         if validacao(request):
-            if form.is_valid(): # tem que preencher todos campos com os tipos que correspondem
-                f = form.save(commit=False)
-                f.nrCPF = request.POST['nrcpfrequerente'].replace('.','').replace('-','')
-                form.save()
-                return HttpResponseRedirect( response_consulta ) 
-    else:
-        form = FormServidor() #gera registro novo
-    
-    #return render_to_response('sicop/restrito/peca_tecnica/cadastro.html',{"form":form,'caixa':caixa,'contrato':contrato,'gleba':gleba}, context_instance = RequestContext(request))
-    return render_to_response('controle/servidor/cadastro.html',{"form":form}, context_instance = RequestContext(request))
+            _nrCPF = request.POST['nrcpf'].replace('.','').replace('-','')
+            if not _nrCPF:
+                _nrCPF = None
+            f_servidor = Tbservidor(
+                    nmservidor = request.POST['nmservidor'],
+                    nmunidade = request.POST['nmunidade'],
+                    nmlotacao = request.POST['nmlotacao'],
+                    cdsiape = request.POST['cdsiape'],
+                    nrcpf = _nrCPF,
+                    dsportariacargo = request.POST['dsportariacargo'],
+                    dsportaria = request.POST['dsportaria'],
+                    nmcargo = request.POST['nmcargo'],
+                    nrtelefone1 = request.POST['nrtelefone1'],
+                    nrtelefone2 = request.POST['nrtelefone2'],
+                    email = request.POST['email'],
+                    dsatividades = request.POST['dsatividades'],
+                    tbdivisao = Tbdivisao.objects.get( pk = request.POST['tbdivisao']),
+                    )
+            f_servidor.save()
+            return HttpResponseRedirect("/controle/restrito/servidor/consulta/")
+    return render_to_response('controle/servidor/cadastro.html',{'divisao':divisao}, context_instance = RequestContext(request))
 
 @login_required
 @user_passes_test( lambda u: verificar_permissao_grupo(u, {'Super','Administrador'}), login_url='/excecoes/permissao_negada/')
@@ -67,27 +73,28 @@ def edicao(request, id):
     ##contrato = Tbcontrato.objects.all()
     ##caixa = Tbcaixa.objects.filter( tbtipocaixa = 2 )
     #gleba = Tbgleba.objects.all()
-    
     instance = get_object_or_404(Tbservidor, id=id)
-        
     if request.method == "POST":
-        form = FormServidor(request.POST,request.FILES,instance=instance)
-        
         if validacao(request):
-            if form.is_valid():
-                print 'passou form is valid'
-                f = form.save(commit=False)
-                print f.nrCPF 
-                f.nrCPF = request.POST['nrcpfrequerente'].replace('.','').replace('-','')
-                form.save()
-                return HttpResponseRedirect("/sicop/restrito/servidor/edicao/"+str(id)+"/")
-    else:
-        form = FormServidor(instance=instance) 
-
-    return render_to_response('controle/servidor/edicao.html',
-                              {"form":form}, 
-                              context_instance = RequestContext(request))
-
+            f_servidor = Tbservidor(
+                    id = instance.id,
+                    nmservidor = request.POST['nmservidor'],
+                    nmunidade = request.POST['nmunidade'],
+                    nmlotacao = request.POST['nmlotacao'],
+                    cdsiape = request.POST['cdsiape'],
+                    nrcpf = request.POST['nrcpf'].replace('.','').replace('-',''),
+                    dsportariacargo = request.POST['dsportariacargo'],
+                    dsportaria = request.POST['dsportaria'],
+                    nmcargo = request.POST['nmcargo'],
+                    nrtelefone1 = request.POST['nrtelefone1'],
+                    nrtelefone2 = request.POST['nrtelefone2'],
+                    email = request.POST['email'],
+                    dsatividades = request.POST['dsatividades'],
+                    tbdivisao = AuthUser.objects.get(pk = request.user.id ).tbdivisao
+                    )
+            f_servidor.save()
+            return HttpResponseRedirect("/controle/restrito/servidor/edicao/"+str(id)+"/")
+    return render_to_response('controle/servidor/edicao.html',{"servidor":instance},context_instance = RequestContext(request))
 
 def relatorio_pdf(request):
     # montar objeto lista com os campos a mostrar no relatorio/pdf
