2627838a0bb96e8b6100d5117f023e8721328d5d
waldenilson.carneiro <waldenilson.carneiro@msls238784.sls.incra.gov.br>
2014-03-11 11:23:35 -0300

    Geração dos relatórios básicos do sistema já utilizando o módulo webodt.

--- SICOP/sicop/restrito/relatorio.py
+++ SICOP/sicop/restrito/relatorio.py
@@ -1,39 +1,42 @@
-from django.http.response import HttpResponse
-from reportlab.platypus.doctemplate import SimpleDocTemplate
-import time
-from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-from reportlab.lib.enums import TA_JUSTIFY, TA_CENTER
-from reportlab.platypus.paragraph import Paragraph
-from reportlab.platypus.flowables import Spacer
-from django.utils.datetime_safe import date
-from reportlab.pdfgen import canvas
-import Canvas
-from reportlab.lib import styles
-from django.shortcuts import render_to_response
-from django.template.context import RequestContext
-from sicop.models import Tbprocessobase, Tbpecastecnicas, Tbgleba,\
-    Tbprocessorural
+
+# -*- coding: UTF-8 -*-
+
+from django.template.context import Context
+from sicop.models import Tbprocessobase, Tbpecastecnicas, \
+    Tbprocessorural, AuthUser
 from sicop.relatorio_base import relatorio_base
 from django.contrib.auth.decorators import permission_required
+import webodt
+from webodt.shortcuts import render_to_response, render_to
+from fileinput import filename
+import os
+from django.http.response import HttpResponse, HttpResponseRedirect
+import shutil
+from TerraLegal import settings
+from django.utils.datetime_safe import datetime
+
 
 #PROCESSOS QUE TEM PECA TECNICA
 @permission_required('sicop.processo_peca_consulta', login_url='/excecoes/permissao_negada/', raise_exception=True)
 def processo_peca(request):
-    
+        
     #buscar os processos que tem o cpf do requerente ligado a uma peca tecnica
-    p_rural = Tbprocessorural.objects.all()
+    p_rural = Tbprocessorural.objects.filter( tbprocessobase__tbdivisao__id = AuthUser.objects.get( pk = request.user.id ).tbdivisao.id )
     p_rural_com_peca = []
     for r in p_rural:
-        if Tbpecastecnicas.objects.all().filter( nrcpfrequerente = r.nrcpfrequerente.replace('.','').replace('-','') ):
+        if Tbpecastecnicas.objects.filter( nrcpfrequerente = r.nrcpfrequerente.replace('.','').replace('-','') ):
             p_rural_com_peca.append( r )
-    #print 'total processos com peca: '+str(len(p_rural_com_peca))
     
+    context = dict(         
+                        titulo='Relatorio Processos com Peca Tecnica',
+                        total=len(p_rural_com_peca),
+                        lista=p_rural_com_peca
+                  )
     
-    lista = Tbprocessobase.objects.all()
-    resp = relatorio_base(request, lista, 'Processo x Peca')
-    return resp
+    return render_to_response('relatorio/processos-com-peca.odt',dictionary=context,format='odt',filename='relatorio-processos-com-peca.odt')
 
 #PECAS TECNICAS POR GLEBAS
+
 @permission_required('sicop.peca_gleba_consulta', login_url='/excecoes/permissao_negada/', raise_exception=True)
 def peca_gleba(request):
     
@@ -45,69 +48,94 @@ def peca_gleba(request):
         glebas.append( obj.tbgleba )
     
     #todas as pecas
-    pecas = Tbpecastecnicas.objects.all()
+#    pecas = Tbpecastecnicas.objects.all()
     for g in glebas:
-        #print 'Gleba: '+str(g.nmgleba)
+        print 'Gleba: '+str(g.nmgleba)
         qtd = 0
         for p in pecas:
             if p.tbgleba.id == g.id:
                 qtd += 1
-        #print 'Total: '+str(qtd)
-    
-    lista = Tbprocessobase.objects.all()
-    resp = relatorio_base(request, lista, 'Peca x Gleba')
-    return resp
+        print 'Total: '+str(qtd)
+
+    context = dict(         
+                        titulo='Relatório das Peças Técnicas por Gleba',
+                        total=len(glebas),
+                        glebas=glebas
+                  )   
+
+    return render_to_response('relatorio/pecas-por-gleba.odt',dictionary=context,format='odt',filename='relatorio-pecas-por-gleba.odt')
 
 #PECAS TECNICAS NAO APROVADAS
+
 @permission_required('sicop.peca_nao_aprovada_consulta', login_url='/excecoes/permissao_negada/', raise_exception=True)
 def peca_nao_aprovada(request):
     
     #buscar as pecas tecnicas com status false
     pecas = Tbpecastecnicas.objects.filter( stpecatecnica = False )
-    #print 'pecas nao aprovadas: '+str(pecas.count())
     
-    lista = Tbprocessobase.objects.all()
-    resp = relatorio_base(request, lista, 'Pecas nao Aprovadas')
-    return resp
-
+    context = dict(         
+                            titulo= 'Relatório das Peças Técnicas não Aprovadas',
+                            total=len(pecas),
+                            lista=pecas
+                        )
+    
+    # requerente, caixa, gleba, area, perimetro, contrato, entrega, dsobservacao
+    
+    return render_to_response('relatorio/pecas-nao-aprovadas.odt',
+                              dictionary=context,format='odt',
+                              filename='relatorio-pecas-nao-aprovadas.odt')
+    
 #PECAS TECNICAS REJEITADAS
+
 @permission_required('sicop.peca_rejeitada_consulta', login_url='/excecoes/permissao_negada/', raise_exception=True)
 def peca_rejeitada(request):
     
     #buscar as pecas tecnicas nao enviadas pra brasilia
     pecas = Tbpecastecnicas.objects.filter( stenviadobrasilia = False )
-    #print 'pecas rejeitadas: '+str(pecas.count())
     
-    lista = Tbprocessobase.objects.all()
-    resp = relatorio_base(request, lista, 'Pecas Rejeitadas')
-    return resp
-
+    context = dict(        
+                            titulo='Relatório das Peças Técnicas rejeitadas',
+                            total=len(pecas),
+                            lista=pecas
+                        )
+    
+    return render_to_response('relatorio/pecas-rejeitadas.odt',dictionary=context,format='odt',filename='relatorio-pecas-rejeitadas.odt')
+    
 #PECAS TECNICAS SEM PROCESSO
+
 @permission_required('sicop.peca_sem_processo_consulta', login_url='/excecoes/permissao_negada/', raise_exception=True)
 def peca_sem_processo(request):
-    
+
     #buscar as pecas tecnicas que nao estao ligadas a um processo
-    pecas = Tbpecastecnicas.objects.all()
+    pecas = Tbpecastecnicas.objects.filter( tbdivisao__id = AuthUser.objects.get( pk = request.user.id ).tbdivisao.id )
     pecas_sem_proc = []
+    
     for p in pecas:
-        if not Tbprocessorural.objects.all().filter( nrcpfrequerente = p.nrcpfrequerente.replace('.','').replace('-','') ):
-            pecas_sem_proc.append( p )
-    #print 'total pecas sem processo: '+str(len(pecas_sem_proc))
-
-
-    lista = Tbprocessobase.objects.all()
-    resp = relatorio_base(request, lista, 'Pecas sem Processos')
-    return resp
+        if not Tbprocessorural.objects.filter( nrcpfrequerente = p.nrcpfrequerente ):
+            pecas_sem_proc.append(p)
+    
+    context = dict(        
+                    titulo='Relatório das Peças Técnicas sem processo',
+                    total=len(pecas_sem_proc),
+                    lista=pecas_sem_proc
+                )
+    
+    return render_to_response('relatorio/pecas-sem-processo.odt',dictionary=context,format='odt',filename='relatorio-pecas-sem-processo.odt')
 
 #PECAS TECNICAS VALIDADAS
+
 @permission_required('sicop.peca_validada_consulta', login_url='/excecoes/permissao_negada/', raise_exception=True)
 def peca_validada(request):
     
     #buscar as pecas tecnicas enviadas pra brasilia
     pecas = Tbpecastecnicas.objects.filter( stenviadobrasilia = True )
-    #print 'pecas validadas: '+str(pecas.count())
+    print 'pecas validadas: '+str(pecas.count())
         
-    lista = Tbprocessobase.objects.all()
-    resp = relatorio_base(request, lista, 'Pecas Validadas')
-    return resp
-
+    context = dict(         
+                    titulo='Relatório das Peças Técnicas validadas',
+                    total=len(pecas),
+                    lista=pecas
+                )
+    
+    return render_to_response('relatorio/pecas-validadas.odt',dictionary=context,format='odt',filename='relatorio-pecas-validadas.odt')
+    
