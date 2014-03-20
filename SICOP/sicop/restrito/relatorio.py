b98ab5eb12bbb9636e233e4e00c5f2c62f9ace0c
waldenilson.carneiro <waldenilson.carneiro@msls238784.sls.incra.gov.br>
2014-03-20 11:55:53 -0300

    Relatorio pecas nao aprovadas no formato *.ods

--- SICOP/sicop/restrito/relatorio.py
+++ SICOP/sicop/restrito/relatorio.py
@@ -4,7 +4,8 @@
 from django.template.context import Context
 from sicop.models import Tbprocessobase, Tbpecastecnicas, \
     Tbprocessorural, AuthUser
-from sicop.relatorio_base import relatorio_base
+from sicop.relatorio_base import relatorio_base, relatorio_ods_base_header,\
+    relatorio_ods_base
 from django.contrib.auth.decorators import permission_required
 import webodt
 from webodt.shortcuts import render_to_response, render_to
@@ -14,6 +15,8 @@ from django.http.response import HttpResponse, HttpResponseRedirect
 import shutil
 from TerraLegal import settings
 from django.utils.datetime_safe import datetime
+from odslib import ODS
+
 
 
 #PROCESSOS QUE TEM PECA TECNICA
@@ -71,19 +74,67 @@ def peca_gleba(request):
 def peca_nao_aprovada(request):
     
     #buscar as pecas tecnicas com status false
-    pecas = Tbpecastecnicas.objects.filter( stpecatecnica = False )
+    pecas = Tbpecastecnicas.objects.filter( stpecatecnica = False ).order_by('nmrequerente')
     
-    context = dict(         
-                            titulo= 'Relatório das Peças Técnicas não Aprovadas',
-                            total=len(pecas),
-                            lista=pecas
-                        )
     
-    # requerente, caixa, gleba, area, perimetro, contrato, entrega, dsobservacao
+    #RELATORIO EM FORMATO ODS
+    nome_relatorio = "relatorio-pecas-nao-aprovadas"
+    titulo_relatorio    = "Relatorio das Pecas Tecnicas nao Aprovadas"
+    planilha_relatorio  = "Pecas nao aprovadas"
+    ods = ODS()
+    sheet = relatorio_ods_base_header(planilha_relatorio, titulo_relatorio, ods)
     
-    return render_to_response('relatorio/pecas-nao-aprovadas.odt',
-                              dictionary=context,format='odt',
-                              filename='relatorio-pecas-nao-aprovadas.odt')
+    sheet.getCell(0,1).setAlignHorizontal('center').stringValue( 'Total: '+str(len(pecas)) ).setFontSize('14pt')
+        
+    # subtitle
+    sheet.getCell(0, 3).setAlignHorizontal('center').stringValue( 'Contrato' ).setFontSize('14pt')
+    sheet.getCell(1, 3).setAlignHorizontal('center').stringValue( 'Entrega' ).setFontSize('14pt')
+    sheet.getCell(2, 3).setAlignHorizontal('center').stringValue( 'Requerente' ).setFontSize('14pt')
+    sheet.getCell(3, 3).setAlignHorizontal('center').stringValue( 'Pasta' ).setFontSize('14pt')
+    sheet.getCell(4, 3).setAlignHorizontal('center').stringValue( 'Area' ).setFontSize('14pt')
+    sheet.getCell(5, 3).setAlignHorizontal('center').stringValue( 'Perimetro' ).setFontSize('14pt')
+    sheet.getCell(6, 3).setAlignHorizontal('center').stringValue( 'Gleba' ).setFontSize('14pt')
+    sheet.getRow(1).setHeight('20pt')
+    sheet.getRow(3).setHeight('20pt')
+    
+    sheet.getColumn(0).setWidth("2in")
+    sheet.getColumn(1).setWidth("2in")
+    sheet.getColumn(2).setWidth("5in")
+    sheet.getColumn(3).setWidth("3.5in")
+    sheet.getColumn(4).setWidth("2in")
+    sheet.getColumn(5).setWidth("2in")
+    sheet.getColumn(6).setWidth("2.5in")
+        
+    #TRECHO PERSONALIZADO DE CADA CONSULTA
+        #DADOS
+    x = 2
+    for obj in pecas:
+        sheet.getCell(0, x+2).setAlignHorizontal('center').stringValue(obj.tbcontrato.nrcontrato)
+        sheet.getCell(1, x+2).setAlignHorizontal('center').stringValue(obj.nrentrega)    
+        sheet.getCell(2, x+2).setAlignHorizontal('center').stringValue(obj.nmrequerente)
+        sheet.getCell(3, x+2).setAlignHorizontal('center').stringValue(obj.tbcaixa.nmlocalarquivo)
+        sheet.getCell(4, x+2).setAlignHorizontal('center').stringValue(obj.nrarea)
+        sheet.getCell(5, x+2).setAlignHorizontal('center').stringValue(obj.nrperimetro)
+        sheet.getCell(6, x+2).setAlignHorizontal('center').stringValue(obj.tbgleba.nmgleba)    
+        x += 1
+        
+    #TRECHO PERSONALIZADO DE CADA CONSULTA     
+       
+    relatorio_ods_base(ods, planilha_relatorio)
+    # generating response
+    response = HttpResponse(mimetype=ods.mimetype.toString())
+    response['Content-Disposition'] = 'attachment; filename='+nome_relatorio+'.ods'
+    ods.save(response)
+    
+    return response
+
+    
+    
+    
+    
+    
+    
+    
     
 #PECAS TECNICAS REJEITADAS
 
