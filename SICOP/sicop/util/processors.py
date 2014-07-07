97c6cea4f7ba7aabc39effa6f1d3fe275855b02f
Administrador <Administrador@msls238784.sls.incra.gov.br>
2014-07-07 12:08:35 -0300

    Formulario de login autenticando direto do template base.

--- SICOP/sicop/util/processors.py
+++ SICOP/sicop/util/processors.py
@@ -15,61 +15,26 @@ from django.http.response import HttpResponseRedirect
 from django.contrib.sites.models import get_current_site
 from django.template.response import TemplateResponse
 
-'''
+
 def login_form(request):
-    form = AuthenticationForm()
-    return {'login_form': form}
-'''
-
-@sensitive_post_parameters()
-@csrf_protect
-@never_cache
-def login_form(request, template_name='registration/login.html',
-          redirect_field_name=REDIRECT_FIELD_NAME,
-          authentication_form=AuthenticationForm,
-          current_app=None, extra_context=None):
-    """
-    Displays the login form and handles the login action.
-    """
-    redirect_to = request.REQUEST.get(redirect_field_name, '')
-
     
+        # if the top login form has been posted
+        if request.method == 'POST' and 'is_top_login_form' in request.POST:
+
+            # validate the form
+            form = AuthenticationForm(data=request.POST)
+            if form.is_valid():
+
+                login(request, form.get_user())
+
+                # if this is the logout page, then redirect to /
+                # so we don't get logged out just after logging in
+                if '/account/logout/' in request.get_full_path():
+                    return HttpResponseRedirect('/')
+
+        else:
+            form = AuthenticationForm(request)    
     
-    if request.method == "POST":
-        form = authentication_form(data=request.POST)
-        if form.is_valid():
+        return {'login_form': form}
 
-            # Ensure the user-originating redirection url is safe.
-            if not is_safe_url(url=redirect_to, host=request.get_host()):
-                redirect_to = resolve_url(settings.LOGIN_REDIRECT_URL)
-
-            # Okay, security check complete. Log the user in.
-            #auth_login(request, form.get_user())
-            
-            login(request, form.get_user())
-
-
-            if request.session.test_cookie_worked():
-                request.session.delete_test_cookie()
-
-            return HttpResponseRedirect(redirect_to)
-    else:
-        form = authentication_form(request)
-
-    request.session.set_test_cookie()
-
-    current_site = get_current_site(request)
-
-    context = {
-        'form': form,
-        redirect_field_name: redirect_to,
-        'site': current_site,
-        'site_name': current_site.name,
-    }
-    if extra_context is not None:
-        context.update(extra_context)
-
-    print 'acessou!'
-    return TemplateResponse(request, template_name, context,
-                            current_app=current_app)
 
