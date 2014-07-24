9a33fef36d463017fc2403bf259bd8ad769374a0
eduardo <edu.saoluis@gmail.com>
2014-07-24 10:07:06 -0300

    Refatoracao SICOP continuacao acerto app web

--- SICOP/web/urls.py
+++ SICOP/web/urls.py
@@ -5,8 +5,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('web.views_publicas',  
     url(r'^terra_legal/', 'terra_legal'),
     url(r'^equipe/', 'equipe'),
-    url(r'^processo_rural/', 'processo_rural'),
-    url(r'^regularizacao_urbana/', 'regularizacao_urbana'),
     url(r'^organizacao/', 'organizacao'),
     
     )
