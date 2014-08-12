b25471dd4a78409c71e891b41e5ee4acf197d4f1
eduardoaguilar <edu.saoluis@gmail.com>
2014-08-12 17:27:41 -0300

    Acerto de calculo de portaria 23

--- TerraLegal/calculo/views.py
+++ TerraLegal/calculo/views.py
@@ -39,29 +39,37 @@ def emissao(request,id):
     
     if modulos > 4:
         indice = 6.75/100
-    if valor_imovel <= 40000:
-        indice = 1.0/100
-    elif valor_imovel > 40000 and valor_imovel <= 100000:
-        indice = 2.0/100
-    elif valor_imovel > 100000:
-        indice = 4.0/100
+    else:
+        if valor_imovel <= 40000:
+            indice = 1.0/100
+        else: 
+            if valor_imovel > 40000 and valor_imovel <= 100000:
+                indice = 2.0/100
+            else: 
+                if valor_imovel > 100000:
+                    indice = 4.0/100
+
+
     '''se ainda nao tiver vencido ha incidencia de  correcao'''
     dias_correcao = hoje - titulado  
     correcao = float(prestacao)*((float(dias_correcao.days)/360.) * indice)
     principal_corrigido = float(prestacao) + correcao
+   
     '''caso tenha vencido, incide juros de 1% ao mes sobre o valor corrigido'''
     if hoje > vencimento:
-        dias_juros = hoje - vencimento + 1
-        juros = (float(dias_juros.days)/30)* (1.0/100.0) * principal_corrigido  
+        dias_juros = hoje - vencimento
+        juros = ((float(dias_juros.days)+1)/30)* (1.0/100.0) * principal_corrigido  
         principal_corrigido = (juros + principal_corrigido)
         juros = "{0:.2f}".format(juros)
 
     #principal_corrigido = principal_corrigido.quantize(Decimal('1,00'))    
+    prestacao = "{0:.2f}".format(prestacao)
     titulado = formatDataToText(titulado)
     vencimento = formatDataToText(vencimento)
     correcao = "{0:.2f}".format(correcao)
     principal_corrigido = "{0:.2f}".format(principal_corrigido)
     ordem = 1
+    indice = indice * 100
 
     return render_to_response('portaria23/calculo.html' ,locals(), context_instance = RequestContext(request))
 
