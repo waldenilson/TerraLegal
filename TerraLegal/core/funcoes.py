7112703e8d44a79aeb31e18b593ec0835db4a59e
Waldenilson Carneiro <wal.denilson@hotmail.com>
2015-01-13 09:32:59 -0300

    Retirado o import do modulo pisa, nao utilizado.

--- TerraLegal/core/funcoes.py
+++ TerraLegal/core/funcoes.py
@@ -30,7 +30,7 @@ from django import http
 from django.template.loader import get_template
 from django.template import Context
 
-import ho.pisa as pisa
+#import ho.pisa as pisa
 import cStringIO as StringIO
 import os
 from django.conf import settings
