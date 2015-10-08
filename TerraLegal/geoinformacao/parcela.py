1528ae3770ffc47188efdeb5b8163143e71dc50f
Waldenilson Carneiro <waldenilsonnovaes@gmail.com>
2015-10-08 14:32:34 -0300

    Geoinformacao. Correcao de erro no exibição de mensagem da consulta de parcelas.

--- TerraLegal/geoinformacao/parcela.py
+++ TerraLegal/geoinformacao/parcela.py
@@ -9,6 +9,7 @@ from django.contrib.auth.decorators import login_required, user_passes_test,\
 from django.http import HttpResponseRedirect
 from django.db.models import Q
 from django.shortcuts import render_to_response, get_object_or_404
+from django.contrib import messages
 
 
 
