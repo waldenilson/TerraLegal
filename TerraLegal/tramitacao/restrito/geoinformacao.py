a6f1cc7d1e22b1cc11b80700fac9f67f67a0f0cb
Waldenilson Carneiro <waldenilsonnovaes@gmail.com>
2015-12-23 13:51:11 -0300

    comentando linhas da view glebas_federais.

--- TerraLegal/tramitacao/restrito/geoinformacao.py
+++ TerraLegal/tramitacao/restrito/geoinformacao.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
-from django.contrib.gis.geos import GEOSGeometry
+#from django.contrib.gis.geos import GEOSGeometry
 import datetime
 
 def glebas_federais(request):
 	print datetime.datetime.now()
-	particular = Roads.objects.filter( regional = 'MA' )
-	assentamento = Roads.objects.filter( regional = 'MA', natureza='Assentamento' )
+	#particular = Roads.objects.filter( regional = 'MA' )
+	#assentamento = Roads.objects.filter( regional = 'MA', natureza='Assentamento' )
 	#sobre_pa = []
 	#for pa in assentamento:
 	#	obj = GEOSGeometry(pa.geom)
@@ -14,9 +14,9 @@ def glebas_federais(request):
 	#		if obj.overlaps(p.geom):
 	#			sobre_pa.append(pa)
 	#			sobre_pa.append(p)
-	print datetime.datetime.now()
+	#print datetime.datetime.now()
 	return render_to_response('sicop/geoinformacao/glebas_federais.html',
-		{'geo':particular}, context_instance = RequestContext(request))
+		{}, context_instance = RequestContext(request))
 
 def openlayers(request):
 	return render_to_response('sicop/geoinformacao/kml.html',
