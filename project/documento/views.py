2a2259d629410c5e110b5d47423699130d3ec403
Waldenilson Carneiro <waldenilsonnovaes@gmail.com>
2015-12-31 16:59:06 -0300

    criacao da pagina com links dos controles dos documentos

--- project/documento/views.py
+++ project/documento/views.py
@@ -1,1 +1,7 @@
 #coding:utf-8
+from django.template.context import RequestContext
+from django.shortcuts import render_to_response
+
+@permission_required('documento.documento_consulta', login_url='/excecoes/permissao_negada/', raise_exception=True)
+def lista(request):
+    return render_to_response('documento/lista.html',{}, context_instance = RequestContext(request))
